3d3cfc705c11acde78768fcfdb0443442ef15f25
Rinat Hayrapetyan <rinat.hairapetian@gmail.com>
2019-05-28 15:45:24 +0400

    Added userList, userItem, UserDetail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-list/user-item/user-item.component';
+import { UserDetailComponent } from './user-list/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserItemComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
