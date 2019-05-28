d18df555712e0f4bcc679032d3797d2cb242a507
Robert Aydinyan <robert.aydinyan2002@gmail.com>
2019-05-28 15:53:42 +0400

    add left-sidebar and top-toolbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-list/user-item/user-item.component';
 import { UserDetailComponent } from './user-list/user-detail/user-detail.component';
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     UserItemComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    TopToolbarComponent,
+    LeftSidebarComponent
   ],
   imports: [
     BrowserModule
