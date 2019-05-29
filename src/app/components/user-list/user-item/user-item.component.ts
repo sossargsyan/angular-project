f13195c587c94dac52899f52f309f3c30315dbec
Rinat Hayrapetyan <rinat.hairapetian@gmail.com>
2019-05-29 19:13:23 +0400

    added mock data to user-list

--- src/app/components/user-list/user-item/user-item.component.ts
+++ src/app/components/user-list/user-item/user-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.scss']
 })
 export class UserItemComponent implements OnInit {
-
+  @Input() user: User;
   constructor() { }
 
   ngOnInit() {
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+  
+  users: User[] = [
+    {
+      name: 'Aram', 
+      surname: 'Aramyan', 
+      email: 'aramaramyan@example.com'
+    },
+    {
+      name: 'Sanasar', 
+      surname: 'Baghdasaryan', 
+      email: 'sanasarbaghdasaryan@example.com'
+    },
+  ];
 
   constructor() { }
 
