a27dabd35f53fc660f631517a0db5008c5dca9ce
Sos Sargsyan <sossargsyan@gmail.com>
2019-05-29 17:54:14 +0400

    Change code structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-list/user-item/user-item.component';
-import { UserDetailComponent } from './user-list/user-detail/user-detail.component';
-import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
-import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserItemComponent } from './components/user-list/user-item/user-item.component';
+import { UserDetailComponent } from './components/user-list/user-detail/user-detail.component';
+import { TopToolbarComponent } from './components/top-toolbar/top-toolbar.component';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 
 @NgModule({
   declarations: [
