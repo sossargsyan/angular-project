726c9192256d0c5c5842c5838d0236057ab8b17e
Robert Aydinyan <robert.aydinyan2002@gmail.com>
2019-06-25 19:17:41 +0400

    create updateUser

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -6,46 +6,52 @@ import { User } from '../models/user.model';
 @Injectable()
 export class UsersService {
 
-    private bigestID = 3;
-    users: User[] = [
-        {
-            id: 1,
-            name: 'Aram',
-            surname: 'Aramyan',
-            email: 'aramaramyan@example.com'
-        },
-        {
-            id: 2,
-            name: 'Sanasar',
-            surname: 'Baghdasaryan',
-            email: 'sanasarbaghdasaryan@example.com'
-        },
-        {
-            id: 3,
-            name: 'Sanasar',
-            surname: 'Baghdasaryan',
-            email: 'sanasarbaghdasaryan@example.com'
-        },
-    ];
+  private bigestID = 3;
+  users: User[] = [
+    {
+      id: 1,
+      name: 'Aram',
+      surname: 'Aramyan',
+      email: 'aramaramyan@example.com'
+    },
+    {
+      id: 2,
+      name: 'Sanasar',
+      surname: 'Baghdasaryan',
+      email: 'sanasarbaghdasaryan@example.com'
+    },
+    {
+      id: 3,
+      name: 'Sanasar',
+      surname: 'Baghdasaryan',
+      email: 'sanasarbaghdasaryan@example.com'
+    },
+  ];
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-    getUsers() {
-        const headers = new HttpHeaders();
-        return this.http.get('http://localhost:3000/api/users', { headers });
-    }
+  getUsers() {
+    const headers = new HttpHeaders();
+    return this.http.get('http://localhost:3000/api/users', {headers});
+  }
 
-    addUser(id: number, name: string, surname: string, email: string) {
-        this.bigestID = id;
-        this.users.push({ id, name, surname, email});
-    }
+  updateUser(user) {
+    const headers = new HttpHeaders();
+    return this.http.put('http://localhost:3000/api/update-user/' + user.id, user, {headers});
+  }
 
-    getUserByID(id: number) {
-        return this.users.find(item => item.id === id);
-    }
+  addUser(id: number, name: string, surname: string, email: string) {
+    this.bigestID = id;
+    this.users.push({id, name, surname, email});
+  }
 
-    getBiggestID() {
-        return this.bigestID;
-    }
+  getUserByID(id: number) {
+    return this.users.find(item => item.id === id);
+  }
+
+  getBiggestID() {
+    return this.bigestID;
+  }
 
 }
